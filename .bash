3c8650635d2b54313367c9e17e4b31ee941bdfb2
Pavithrakende <143390977+Pavithrakende@users.noreply.github.com>
2024-07-08 21:53:55 +0530

    .bash

---
+++ .bash
@@ -0,0 +1,9 @@
+
+
+//1.Connect the DC motors to the motor driver board and then connect the motor driver to the GPIO pins on the Raspberry Pi as defined in the code.
+//2.Connect the night vision camera to the Raspberry Pi.
+//3.Run the Flask app to start live streaming:
+         python3 app//to run
+//4.Open a web browser and navigate to http://<Raspberry_Pi_IP>:5000 to view the live stream from the camera.
+
+//5.Control the robot's movement using the motor control functions (forward, backward, stop). You can integrate these functions into the Flask app or use another method (e.g., a separate script or web interface) to control the robot.
